031a8f17d8555fb7797975d23836ae51e2cd1d0e
ChuDinhKlopp <chutuanduc0505@gmail.com>
2024-08-05 11:13:50 +0700

    Fixed accumulated error in matMul

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ducb/Documents/C++/forward-kinematics/src
+# Install script for directory: /home/ducb/Local-Git-Repos/fk_PoE.c/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ducb/Documents/C++/forward-kinematics/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ducb/Local-Git-Repos/fk_PoE.c/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
